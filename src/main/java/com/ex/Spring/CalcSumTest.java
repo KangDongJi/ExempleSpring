7a967c240538bae6ef5b96dc6606d0a6361fc281
KangDognJi <bong6419@gmail.com>
2012-11-30 15:58:53 +0900

    edit

--- src/main/java/com/ex/Spring/CalcSumTest.java
+++ src/main/java/com/ex/Spring/CalcSumTest.java
@@ -15,12 +15,12 @@ public class CalcSumTest {
 		this.calculator = new Calculator();
 		this.numFilepath = getClass().getResource("numbers.txt").getPath();
 	}
-	@Test public void sumOfNumbers() throws IOException{
+//	@Test public void sumOfNumbers() throws IOException{
 //		assertThat(calculator.calcSum(this.numFilepath), is(10));
-	}
-	@Test public void multiplyOfNumbers() throws IOException{
+//	}
+//	@Test public void multiplyOfNumbers() throws IOException{
 //		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
-	}
+//	}
 	@Test public void concatenateStrings() throws IOException{
 		assertThat(calculator.concatenate(this.numFilepath), is("1234"));
 	}
